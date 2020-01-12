736f74c75ebb4b53a7d5775cbab84e5d3f36e20e
zhaojintian <729234283@qq.com>
2020-01-12 23:14:01 +0800

    update

---
+++ utils/create-store/actions.js
@@ -0,0 +1,32 @@
+export default ({ types, Model }) => {
+  return {
+    async getList ({ commit }, { showLoading, showError, query }) {
+      const { data } = await new Model().GET({ showLoading, showError, query })
+      commit(types.GET_LIST, { data })
+      return data
+    },
+
+    async getDetail ({ commit }, { showLoading, showError, id }) {
+      const { data } = await new Model().GET({ showLoading, showError, id })
+      commit(types.GET_DETAIL, { data })
+      return data
+    },
+
+    post ({ commit }, { showLoading, showError, body }) {
+      return new Model().POST({ showLoading, showError, body })
+    },
+
+    put ({ commit }, { showLoading, showError, id, body }) {
+      return new Model().PUT({ showLoading, showError, id, body })
+    },
+
+    del ({ commit }, { showLoading, showError, id }) {
+      return new Model().DELETE({ showLoading, showError, id })
+    },
+
+    postAction ({ commit }, { showLoading, showError, query, body }) {
+      return new Model().addPath('actions').
+        POST({ showLoading, showError, query, body })
+    }
+  }
+}
