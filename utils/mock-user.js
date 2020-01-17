5ffceb3dce65f309b4769fd448392ebfa17d8431
zhaojintian <729234283@qq.com>
2020-01-18 06:06:42 +0800

    update

---
+++ utils/mock-user.js
@@ -0,0 +1,25 @@
+import wxb from 'wx-bridge'
+
+// #ifdef H5
+const { openId, user, token } = require('@/mock-user.json')
+// #endif
+
+// #ifndef H5
+const openId = ''
+const user = {}
+const token = ''
+// #endif
+
+const OPEN_ID = 'openId'
+const USER = 'user'
+const TOKEN = 'token'
+
+export default {
+  login () {
+    wxb.setStorageSync(USER, user)
+    wxb.setStorageSync(TOKEN, `Bearer ${token}`)
+  },
+  setOpenId () {
+    wxb.setStorageSync(OPEN_ID, openId)
+  }
+}
