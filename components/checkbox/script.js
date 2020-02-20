5823929450b5813fd06ce6a7e4eb760479bd4477
zhaojintian <729234283@qq.com>
2020-02-21 00:55:43 +0800

    update

--- components/checkbox/script.js
+++ components/checkbox/script.js
@@ -1,9 +1,12 @@
-export default {
-  name: 'c-checkbox',
+import Vue from "vue";
+import { Component } from "vue-property-decorator";
+
+@Component({
   props: {
     checked: {
       type: Boolean,
       default: false
     }
   }
-}
+})
+export default class Checkbox extends Vue {}
