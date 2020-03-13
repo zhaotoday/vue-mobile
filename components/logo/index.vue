9b60c8241a911bfa03554cd4010bd25a5deb620e
zhaojintian <729234283@qq.com>
2020-03-13 11:21:57 +0800

    update

--- components/logo/index.vue
+++ components/logo/index.vue
@@ -1,6 +1,9 @@
 <template>
   <div class="c-logo">
-    <div class="c-icon c-icon--logo" :class="widthModifier"></div>
+    <div
+      class="c-icon c-icon--logo"
+      :class="[widthModifier, heightModifier]"
+    ></div>
     <div class="c-logo__title fs32">{{ name }}</div>
   </div>
 </template>
