2bb71a0f8cbc3c3514cfdd033d5cd42bc3f368a3
zhaojintian <729234283@qq.com>
2020-03-17 23:01:57 +0800

    update

--- mixins/wx-user.js
+++ mixins/wx-user.js
@@ -23,7 +23,7 @@ export default class WxUserMixin extends Vue {
   async refreshWxUserInfo() {
     if (this.$auth.loggedIn()) {
       const {
-        data: { nickName, avatarUrl, phoneNumber, name = "", extra = {} }
+        data: { nickName, avatarUrl, avatarId, phoneNumber, name = "", extra = {} }
       } = await this.$store.dispatch("wx/wxUsers/postAction", {
         showError: false,
         action: "getUserInfo"
@@ -32,6 +32,7 @@ export default class WxUserMixin extends Vue {
       this.$auth.set({
         nickName,
         avatarUrl,
+        avatarId,
         phoneNumber,
         name,
         extra
