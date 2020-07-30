1b8350c8d19b0298f53fb4d097cb87b78f8614c7
zhao <729234283@qq.com>
2020-07-30 14:25:53 +0800

    u

--- components/filter/index.vue
+++ components/filter/index.vue
@@ -1,6 +1,6 @@
 <template>
   <picker :range="range" :value.sync="value" @change="handleChange">
-    <div class="c-filter bgc11 c35 fs24 u-tac">
+    <div class="c-filter bgc11 c37 fs24 u-tac">
       <span>{{ array[value].name || placeholder }}</span>
       <i class="c-icon c-icon--drop"></i>
     </div>
