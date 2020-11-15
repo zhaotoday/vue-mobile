a15da0e19d981a0499c7a887349e0a7635f610b5
729234283 <729234283@qq.com>
2020-11-15 15:59:56 +0800

    u

--- store/modules/wx/wx-users.js
+++ store/modules/wx/wx-users.js
@@ -1,4 +1,28 @@
 import createStore from "../../../utils/create-store";
 import Model from "../../../models/wx/wx-users";
 
-export default createStore({ Model });
+export default createStore({
+  Model,
+  state: {
+    user: {
+      extra: {}
+    }
+  },
+  types: {
+    SET: null
+  },
+  mutations({ types }) {
+    return {
+      [types.SET](state, { key, value }) {
+        state[key] = value;
+      }
+    };
+  },
+  actions({ types }) {
+    return {
+      set({ commit }, { key, value }) {
+        commit(types.SET, { key, value });
+      }
+    };
+  }
+});
