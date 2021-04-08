5f9a92be2dd0587c0b09abdf742f4b4a7c34cad3
729234283 <729234283@qq.com>
2021-04-08 21:13:47 +0800

    update

---
+++ components/area-picker/index.vue
@@ -0,0 +1,16 @@
+<template>
+  <picker
+    class="c-list__value"
+    mode="multiSelector"
+    :range="range"
+    :value="values"
+    range-key="name"
+    @change="onChange"
+    @columnchange="onColumnChange"
+  >
+    <div v-if="!names" class="c37">{{ placeholder }}</div>
+    <div v-else>{{ names }}</div>
+  </picker>
+</template>
+
+<script src="./script.js"></script>
