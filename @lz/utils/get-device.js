cea85a8db8a43d045ea51be9c234d9b6645efcab
729234283 <729234283@qq.com>
2021-04-21 10:31:21 +0800

    update"

--- @lz/utils/get-device.js
+++ @lz/utils/get-device.js
@@ -1,5 +1,5 @@
-export const device = {
+export const getDevice = () => ({
   code: plus.device.uuid,
   systemType: `${plus.os.name}_${plus.os.version}`,
   phoneType: `${plus.device.vendor}_${plus.device.model}`,
-};
+});
