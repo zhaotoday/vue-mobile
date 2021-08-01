0f22b5297482467855e1a9f9bd2e9cbaf7162c6b
zhaojintian <729234283@qq.com>
2021-08-01 16:35:50 +0800

    update

---
+++ @lr/composables/use-user.js
@@ -0,0 +1,19 @@
+export const useUser = () => {
+  const wxMpLogin = () => {};
+
+  const wxOaLogin = () => {};
+
+  const wxAppLogin = () => {};
+
+  const qqWebLogin = () => {};
+
+  const qqAppLogin = () => {};
+
+  return {
+    wxMpLogin,
+    wxOaLogin,
+    wxAppLogin,
+    qqWebLogin,
+    qqAppLogin,
+  };
+};
