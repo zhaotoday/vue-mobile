0987b76d770edb1061cdccabd718786f73967d97
zhaojintian <729234283@qq.com>
2021-08-02 21:58:47 +0800

    upate

--- @lr/composables/use-form-validators.js
+++ @lr/composables/use-form-validators.js
@@ -38,7 +38,7 @@ export const useFormValidators = () => {
         message: message || `${label}格式错误`,
       };
     },
-    phoneNumberOrEmail({ message = "" } = {}) {
+    phoneNumberOrEmail({ label = "账号", message = "" } = {}) {
       const phoneNumberPattern = /^1\d{2}\s?\d{4}\s?\d{4}$/;
       const emailPattern =
         /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
@@ -47,7 +47,7 @@ export const useFormValidators = () => {
         validator(rule, value) {
           return phoneNumberPattern.test(value) || emailPattern.test(value);
         },
-        message: message || "请输入手机号或邮箱",
+        message: message || `${label}格式错误（必须是手机号或邮箱）`,
       };
     },
     password({ label = "密码", message = "" } = {}) {
