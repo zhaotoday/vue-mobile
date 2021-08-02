c51de700f6bbb33c2d84ad479cd10d42f1d75f2f
zhaojintian <729234283@qq.com>
2021-08-02 19:00:54 +0800

    update

--- @lr/composables/use-form-validators.js
+++ @lr/composables/use-form-validators.js
@@ -5,40 +5,40 @@ export const useFormValidators = () => {
         validator(rule, value) {
           return !value || /^[1-9]\d{7}$/.test(value);
         },
-        message: message || "请填写8位数字ID",
+        message: message || "请输入8位数字ID",
       };
     },
-    account() {
+    account({ label = "账号", message = "" } = {}) {
       return {
         pattern: /[a-zA-Z0-9_-]{8,20}/,
-        message: "账号格式错误",
+        message: message || `${label}格式错误`,
       };
     },
-    nickName() {
+    nickName({ label = "昵称", message = "" } = {}) {
       return {
         pattern: /^[\\w\u4e00-\u9fa5]{2,8}$/,
-        message: "昵称格式错误",
+        message: message || `${label}格式错误`,
       };
     },
     required({ label, message } = {}) {
       return {
         required: true,
-        message: message || `请填写${label}`,
+        message: message || `请输入${label}`,
       };
     },
-    phoneNumber({ label = "手机号" } = {}) {
+    phoneNumber({ label = "手机号", message = "" } = {}) {
       return {
         pattern: /^1\d{2}\s?\d{4}\s?\d{4}$/,
-        message: `${label}格式错误`,
+        message: message || `${label}格式错误`,
       };
     },
-    email({ label = "邮箱" } = {}) {
+    email({ label = "邮箱", message = "" } = {}) {
       return {
         type: "email",
-        message: `${label}格式错误`,
+        message: message || `${label}格式错误`,
       };
     },
-    phoneNumberOrEmail() {
+    phoneNumberOrEmail({ message = "" } = {}) {
       const phoneNumberPattern = /^1\d{2}\s?\d{4}\s?\d{4}$/;
       const emailPattern =
         /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
@@ -47,19 +47,19 @@ export const useFormValidators = () => {
         validator(rule, value) {
           return phoneNumberPattern.test(value) || emailPattern.test(value);
         },
-        message: postMessag,
+        message: message || "请输入手机号或邮箱",
       };
     },
-    password({ label = "密码" } = {}) {
+    password({ label = "密码", message = "" } = {}) {
       return {
         pattern: /^(?![0-9]+$)(?![a-zA-Z]+$)[0-9A-Za-z]{6,16}$/,
-        message: `${label}格式错误`,
+        message: message || `${label}格式错误`,
       };
     },
-    captcha({ label = "验证码" } = {}) {
+    captcha({ label = "验证码", message = "" } = {}) {
       return {
         len: 6,
-        message: `${label}格式错误`,
+        message: message || `${label}格式错误`,
       };
     },
   };
