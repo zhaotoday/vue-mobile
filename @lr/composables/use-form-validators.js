ef0e349c0d84da932d24b60ab6626ae569b4723d
zhaojintian <729234283@qq.com>
2021-08-07 15:37:05 +0800

    update

--- @lr/composables/use-form-validators.js
+++ @lr/composables/use-form-validators.js
@@ -1,6 +1,8 @@
+import AsyncValidator from "async-validator";
+
 export const useFormValidators = () => {
   return {
-    userId({ message = "" } = {}) {
+    isUserId({ message = "" } = {}) {
       return {
         validator(rule, value) {
           return !value || /^[1-9]\d{7}$/.test(value);
@@ -8,37 +10,37 @@ export const useFormValidators = () => {
         message: message || "请输入8位数字ID",
       };
     },
-    account({ label = "账号", message = "" } = {}) {
+    isAccount({ label = "账号", message = "" } = {}) {
       return {
         pattern: /[a-zA-Z0-9_-]{8,20}/,
         message: message || `${label}格式错误`,
       };
     },
-    nickName({ label = "昵称", message = "" } = {}) {
+    isNickName({ label = "昵称", message = "" } = {}) {
       return {
         pattern: /^[\\w\u4e00-\u9fa5]{2,8}$/,
         message: message || `${label}格式错误`,
       };
     },
-    required({ label, message } = {}) {
+    isRequired({ label, message } = {}) {
       return {
         required: true,
         message: message || `请输入${label}`,
       };
     },
-    phoneNumber({ label = "手机号", message = "" } = {}) {
+    isPhoneNumber({ label = "手机号", message = "" } = {}) {
       return {
         pattern: /^1\d{2}\s?\d{4}\s?\d{4}$/,
         message: message || `${label}格式错误`,
       };
     },
-    email({ label = "邮箱", message = "" } = {}) {
+    isEmail({ label = "邮箱", message = "" } = {}) {
       return {
         type: "email",
         message: message || `${label}格式错误`,
       };
     },
-    phoneNumberOrEmail({ label = "账号", message = "" } = {}) {
+    isPhoneNumberOrEmail({ label = "账号", message = "" } = {}) {
       const phoneNumberPattern = /^1\d{2}\s?\d{4}\s?\d{4}$/;
       const emailPattern =
         /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
@@ -50,17 +52,35 @@ export const useFormValidators = () => {
         message: message || `${label}格式错误（必须是手机号或邮箱）`,
       };
     },
-    password({ label = "密码", message = "" } = {}) {
+    isPassword({ label = "密码", message = "" } = {}) {
       return {
         pattern: /^(?![0-9]+$)(?![a-zA-Z]+$)[0-9A-Za-z]{6,16}$/,
         message: message || `${label}格式错误`,
       };
     },
-    captcha({ label = "验证码", message = "" } = {}) {
+    isCaptcha({ label = "验证码", message = "" } = {}) {
       return {
         len: 6,
         message: message || `${label}格式错误`,
       };
+    },
+    async validate(cForm) {
+      await new AsyncValidator(cForm.rules).validate(
+        cForm.model,
+        async (errors) => {
+          cForm.errors = {};
+
+          errors.forEach((error) => {
+            cForm.errors[error.field] = error.message;
+          });
+
+          if (errors) {
+            return Promise.reject(errors);
+          } else {
+            return Promise.resolve();
+          }
+        }
+      );
     },
   };
 };
