b493424898ae25b1e0cb9d5a01b1646c8bffc89b
zhaojintian <729234283@qq.com>
2021-10-03 10:56:43 +0800

    u

--- composables/use-pagination.js
+++ composables/use-pagination.js
@@ -2,15 +2,15 @@ import { reactive } from "@vue/composition-api";
 
 export const usePagination = ({ pageSize = 10 } = {}) => {
   const cPagination = reactive({
-    $offset: 0,
-    $limit: pageSize,
+    offset: 0,
+    limit: pageSize,
     finished: false,
     lastPageItems: [],
   });
 
   const resetPagination = () => {
-    cPagination.$offset = 0;
-    cPagination.$limit = pageSize;
+    cPagination.offset = 0;
+    cPagination.limit = pageSize;
     cPagination.finished = false;
     cPagination.lastPageItems = [];
   };
@@ -18,7 +18,7 @@ export const usePagination = ({ pageSize = 10 } = {}) => {
   const setPagination = () => {
     return new Promise((resolve, reject) => {
       if (cPagination.lastPageItems.length > 0) {
-        cPagination.$offset += pageSize;
+        cPagination.offset += pageSize;
         resolve();
       } else {
         cPagination.finished = true;
