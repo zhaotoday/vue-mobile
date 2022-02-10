9e8f4e0cd8d11c4e531da94569c68156772bccb9
zhaojintian <729234283@qq.com>
2022-02-10 15:03:49 +0800

    u

---
+++ components/form/components/item/index.vue
@@ -0,0 +1,9 @@
+<template>
+  <li class="c-form__item">
+    <label>{{ label }}</label>
+    <slot name="default" />
+    <slot name="append" />
+  </li>
+</template>
+
+<script src="./script.js"></script>
---
+++ components/form/components/item/script.js
@@ -0,0 +1,6 @@
+export default {
+  props: {
+    label: String,
+    prop: String,
+  },
+};
