cd6c5b8d00cc3720b072e10756498bfedcfb0517
zhaojintian <729234283@qq.com>
2022-02-15 13:12:08 +0800

    ou

--- components/form/index.vue
+++ components/form/index.vue
@@ -1,7 +1,9 @@
 <template>
-  <div :class="$bem.box.$">
+  <div :class="[$bem.box.$, customClass]">
     <ul class="c-form c-form--inbox has-label">
       <slot />
     </ul>
   </div>
 </template>
+
+<script src="./script.js"></script>
---
+++ components/form/script.js
@@ -0,0 +1,5 @@
+export default {
+  props: {
+    customClass: [String, Object],
+  },
+};
