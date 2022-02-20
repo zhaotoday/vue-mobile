ec168027db536161b5a21562c6899b376a18f61d
zhaojintian <729234283@qq.com>
2022-02-20 08:31:27 +0800

    u

--- @lr/composables/use-wx-mp.js
+++ @lr/composables/use-wx-mp.js
@@ -1,16 +1,39 @@
 import wx from "wx-bridge";
 
 export const useWxMp = () => {
+  const state = {
+    loginTimer: null,
+    loginCode: "",
+  };
+
   const getUserProfileAndLogin = async () => {
     const { iv, encryptedData } = await wx.getUserProfile({
       desc: "完善用户资料",
     });
-    const { code } = await wx.login();
 
-    return { code, iv, encryptedData };
+    return { code: state.code, iv, encryptedData };
+  };
+
+  const getLoginCode = async () => {
+    const { code } = await wx.login();
+    state.code = code;
+  };
+
+  const onRefreshLoginCode = async () => {
+    await getLoginCode();
+
+    state.loginTimer = setInterval(async () => {
+      await getLoginCode();
+    }, 5 * 60 * 1000);
+  };
+
+  const offRefreshLoginCode = () => {
+    clearInterval(state.loginCode);
   };
 
   return {
     getUserProfileAndLogin,
+    onRefreshLoginCode,
+    offRefreshLoginCode,
   };
 };
