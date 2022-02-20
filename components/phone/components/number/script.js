d8e1b712b622fb290a550dfbb753e41952b22b48
zhaojintian <729234283@qq.com>
2022-02-20 16:33:16 +0800

    u

---
+++ components/phone/components/number/script.js
@@ -0,0 +1,42 @@
+import wx from "wx-bridge";
+import { reactive } from "@vue/composition-api";
+import { usersApi } from "../../../../@lr/apis/client/users";
+import { useUsers } from "../../../../@lr/composables/use-users";
+import { useHelpers } from "../../../../@lr/composables/use-helpers";
+
+export default {
+  setup() {
+    const { getUserInfo } = useUsers();
+
+    const cModal = reactive({
+      visible: false,
+    });
+
+    const show = () => {
+      cModal.visible = true;
+    };
+
+    const onGetPhoneNumber = async (e) => {
+      cModal.visible = false;
+
+      if (!e.errMsg) {
+        await usersApi.post({
+          action: "getWxPhoneNumber",
+          body: { code: e.detail.code },
+        });
+
+        await getUserInfo();
+        wx.showToast({ title: "登陆成功" });
+        await useHelpers().sleep(1500);
+      }
+
+      wx.navigateBack();
+    };
+
+    return {
+      cModal,
+      show,
+      onGetPhoneNumber,
+    };
+  },
+};
